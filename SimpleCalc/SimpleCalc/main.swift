019aaa46d6bf3b5de9ac91f84917003d7452a2e4
Gabe <gebizar@uw.edu>
2019-01-15 16:30:54 -0800

    completed hw2

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,6 +10,57 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        var empArr = [Int]()
+        var returnVal = 0
+        for arg in args {
+            switch arg {
+            case "+":
+                returnVal = Int(args[0])! + Int(args[2])!
+            case "-":
+                returnVal = Int(args[0])! - Int(args[2])!
+            case "/":
+                returnVal = Int(args[0])! / Int(args[2])!
+            case "%":
+                var mod = Int(args[0])!
+                while mod - Int(args[2])! >= 0 {
+                    mod -= Int(args[2])!
+                }
+                returnVal = mod
+            case "*":
+                return Int(args[0])! * Int(args[2])!
+            case "count":
+                return args.count - 1
+            case "fact":
+                if empArr.count == 1 {
+                    var factorial = empArr[0]
+                    if factorial > 1 {
+                        for ind in 1...(empArr[0] - 1) {
+                            factorial *= ind
+                            print("FACT:", ind, factorial)
+                        }
+                    }
+                    returnVal = factorial
+                } else {
+                    returnVal = 0
+                }
+            case "avg":
+                var sum = 0
+                for val in empArr {
+                    sum += val
+                }
+                var mean = 0
+                if args.count > 1 {
+                    mean = sum / (args.count - 1)
+                } else {
+                     mean = sum
+                }
+                returnVal = mean
+            default:
+                empArr.append(Int(arg)!)
+            }
+            
+        }
+        return returnVal
     }
     
     public func calculate(_ arg: String) -> Int {
